3b13d06376bda703fd5065c64b24e578139ba5e9
Nadya Prichko <toilspearl@yandex.ru>
2022-06-09 10:48:00 +0300

    fixed

--- Calc/Calc/settings.py
+++ Calc/Calc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'sqrroot_calc.SqrrootCalcConfig',
+    'sqrroot_calc.apps.SqrrootCalcConfig',
 ]
 
 MIDDLEWARE = [
